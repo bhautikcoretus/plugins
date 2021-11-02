c640a7a5ef57dd67ad943ebcaed288f47a95ea46
bhautikcoretus <bhautik.keraliya@coretus.com>
2021-11-02 18:42:37 +0530

    Update in_app_purchase_ios_platform.dart

--- packages/in_app_purchase/in_app_purchase_ios/lib/src/in_app_purchase_ios_platform.dart
+++ packages/in_app_purchase/in_app_purchase_ios/lib/src/in_app_purchase_ios_platform.dart
@@ -71,7 +71,7 @@ class InAppPurchaseIosPlatform extends InAppPurchasePlatform {
     print('Bhautik : buyNonConsumable');
     await _skPaymentQueueWrapper.addPayment(SKPaymentWrapper(
         productIdentifier: purchaseParam.productDetails.id,
-        quantity: (purchaseParam.quantity == null) ? 1 : purchaseParam.quantity,
+        quantity: purchaseParam.quantity,
         applicationUsername: purchaseParam.applicationUserName,
         simulatesAskToBuyInSandbox: (purchaseParam is AppStorePurchaseParam)
             ? purchaseParam.simulatesAskToBuyInSandbox
